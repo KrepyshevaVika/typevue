71b317b910275bd89a07600b455efe1930ad2ef9
Vika <vkrepysheva@inobitec.com>
2018-10-18 18:40:29 +0300

    typescript

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,63 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { StoreOptions, MutationTree, Module, ActionTree, GetterTree } from 'vuex'
+import { IRootState, INode } from './types'
+import { getAllNodes } from './services/service';
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
+const GET_SUCCESS = 'GET_SUCCESS';
+const MESSAGE_ERROR = 'MESSAGE_ERROR';
+/*
+const state: IRootState = {
+  nodes: [],
+  infoAboutNode: {},
+  messageError: "",
+};*/
 
-  },
-  mutations: {
-
-  },
-  actions: {
-
+const actions: ActionTree<IRootState, IRootState> = {
+  getAll({ commit }): any {
+    getAllNodes().then((data: Array<INode>) => {
+      commit('GET_SUCCESS', data);
+    }).catch((error: string) => {
+      commit('MESSAGE_ERROR', error);
+    });
   }
-})
+};
+
+const mutations: MutationTree<IRootState> = {
+  [GET_SUCCESS] (state, nodes: Array<INode>) {
+    console.log(state)
+    state.nodes = nodes;
+    console.log(state.nodes)
+  },
+  [MESSAGE_ERROR] (state, msgError: string) {
+    state.messageError = msgError;
+  },
+};
+
+const getters: GetterTree<IRootState, IRootState> = {
+  all(state): Array<INode> {
+      const { nodes } = state;
+      return nodes;
+  }
+};
+
+export const profile: Module<IRootState, IRootState> = {
+  actions,
+  getters,
+ // state,
+  mutations
+};
+
+const store: StoreOptions<IRootState> = {
+  state: {
+    nodes: [],
+    infoAboutNode: {},
+    messageError: "",
+  },
+  modules: {
+    profile
+  }
+};
+
+export default new Vuex.Store<IRootState>(store);
